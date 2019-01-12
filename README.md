e5c43d5ea29dda9d15b015caafaed036ec24fe7a
VetoPlayer <andrewlloys@gmail.com>
2019-01-12 11:22:14 +0000

    [init] Hello world
    Hello World Program Definition and a basic introduction

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rust
 My Rust Exercises
+
+In this repository I'll report my exercises for the series 'Learning Rust in
+X hours'
+
+Learning Resources and References:
+
+https://doc.rust-lang.org/book/
+
---
+++ helloworld.rs
@@ -0,0 +1,15 @@
+// Welcome to Rust!
+// In order to download all the developers tool:
+// curl https://sh.rustup.rs -sSf | sh and then restart you bash
+
+// Rust programs end with .rs
+
+// To compile this: rustc helloworld.rs
+// rustc is the rust compiler, it should behave in a similar fashion w.r.t.
+// clang or gcc
+fn main(){
+    println!("Hello, World!");
+}
+
+
+// The question mark denotes a Macro call. TODO
